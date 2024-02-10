f27ee76d3dd1830c99c2395e11baa9870addc704
wenxuan-pan <panwenxuan42@gmail.com>
2024-02-10 21:52:56 +1100

    added descope

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth/next"
+import { NextAuthOptions } from "next-auth"
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.SECRET,
+  providers: [
+    {
+      id: "descope",
+      name: "Descope",
+      type: "oauth",
+      wellKnown: `https://api.descope.com/${process.env.DESCOPE_CLIENT_ID}/.well-known/openid-configuration`,
+      authorization: { params: { scope: "openid email profile" } },
+      idToken: true,
+      clientId: process.env.DESCOPE_CLIENT_ID,
+      clientSecret: process.env.DESCOPE_CLIENT_SECRET,
+      checks: ["pkce", "state"],
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture
+        }
+      }
+    }
+  ]
+}
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
