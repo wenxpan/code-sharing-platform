bba71e1fa4e2ec412eaff7372e05dc3a56d6862e
wenxuan-pan <panwenxuan42@gmail.com>
2024-02-11 20:20:23 +1100

    added dashboard routes

---
+++ src/app/dashboard/business/page.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+interface BusinessDashboardProps {}
+
+const BusinessDashboard: React.FC<BusinessDashboardProps> = () => {
+  return <>business dashboard</>
+}
+
+export default BusinessDashboard
