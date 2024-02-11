b47818bd3edfcb6765db905438096a0faa593f80
wenxuan-pan <panwenxuan42@gmail.com>
2024-02-11 22:07:13 +1100

    added convex

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,18 @@
 
 import { SessionProvider } from "next-auth/react"
 import { NextUIProvider } from "@nextui-org/react"
+import { ConvexProvider, ConvexReactClient } from "convex/react"
 import { useRouter } from "next/navigation"
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter()
   return (
     <NextUIProvider navigate={router.push}>
-      <SessionProvider>{children}</SessionProvider>
+      <SessionProvider>
+        <ConvexProvider client={convex}>{children}</ConvexProvider>
+      </SessionProvider>
     </NextUIProvider>
   )
 }
