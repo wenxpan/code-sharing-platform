a5018dbd285f223442dc3eb694e41c645768e110
wenxuan-pan <panwenxuan42@gmail.com>
2024-02-21 17:27:59 +1100

    added nav items to avatar dropdown

--- src/components/AvatarDropDown.tsx
+++ src/components/AvatarDropDown.tsx
@@ -9,6 +9,7 @@ import {
 import { User } from "@nextui-org/user"
 import { Doc } from "@convex/_generated/dataModel"
 import { redirect } from "next/navigation"
+import { useAppUser } from "@/lib/useAppUser"
 
 export const AvatarDropDown = ({ user }: { user: Doc<"users"> }) => {
   const sdk = useDescope()
@@ -22,17 +23,43 @@ export const AvatarDropDown = ({ user }: { user: Doc<"users"> }) => {
     return null
   }
 
-  // TODO: items for employee and admin
-  const coderItems = [
+  const employee = [
+    {
+      key: "posted-jobs",
+      label: "Posted Jobs",
+      href: "/dashboard/business/jobs",
+    },
+  ]
+  const admin = [
+    {
+      key: "manage-employees",
+      label: "Manage Employees",
+      href: "/dashboard/business/employees",
+    },
+  ]
+  const coder = [
+    { key: "profile", label: "My Profile", href: "/profile/coder" },
+    { key: "new-project", label: "New Project", href: "/projects/new" },
+    {
+      key: "projects",
+      label: "My Projects",
+      href: "/dashboard/coder/projects",
+    },
+    { key: "jobs", label: "Applied Jobs", href: "/dashboard/coder/jobs" },
+  ]
+  const specificItems =
+    user.role === "coder"
+      ? coder
+      : user.role === "businessEmployee"
+      ? employee
+      : [...admin, ...employee]
+  const navItems = [
     {
       key: "dashboard",
       label: "Dashboard",
-      href: "/dashboard/coder",
+      href: "/dashboard/",
     },
-    { key: "profile", label: "My Profile", href: "/profile/coder" },
-    { key: "new-project", label: "New Project", href: "/projects/new" },
-    { key: "projects", label: "My Projects", href: "/projects" },
-    { key: "jobs", label: "Applied Jobs", href: "/jobs" },
+    ...specificItems,
     { key: "sign-out", label: "Sign Out" },
   ]
 
@@ -50,11 +77,11 @@ export const AvatarDropDown = ({ user }: { user: Doc<"users"> }) => {
           name={user.name}
         />
       </DropdownTrigger>
-      <DropdownMenu aria-label="User Actions" variant="flat" items={coderItems}>
+      <DropdownMenu aria-label="User Actions" variant="flat" items={navItems}>
         {(item) => (
           <DropdownItem
             key={item.key}
-            href={item.href}
+            href={item?.href}
             color={item.key === "sign-out" ? "danger" : "default"}
             className={item.key === "sign-out" ? "text-danger" : ""}
             onClick={() => item.key === "sign-out" && handleLogout()}
