cece42af5436f3eaa0e89f1bb817654b3993108a
wenxuan-pan <panwenxuan42@gmail.com>
2024-02-21 15:01:19 +1100

    added /dashboard/coder

---
+++ src/app/dashboard/coder/projects/page.tsx
@@ -0,0 +1,40 @@
+"use client"
+import { ProjectCardList } from "@/components/ProjectCard"
+import { useAppUser } from "@/lib/useAppUser"
+import { api } from "@convex/_generated/api"
+import { Spinner } from "@nextui-org/react"
+import { useQuery } from "convex/react"
+import { redirect } from "next/navigation"
+import React from "react"
+
+interface CoderProjectsPageProps {}
+
+const CoderProjectsPage: React.FC<CoderProjectsPageProps> = () => {
+  const { status, user } = useAppUser()
+  const projects = useQuery(api.projects.getProjectByOwner, {
+    owner: user?._id,
+  })
+
+  if (status === "unauthenticated") {
+    redirect("/dashboard")
+  }
+
+  if (!user) {
+    return <Spinner />
+  }
+
+  return (
+    <>
+      <h1 className="font-bold text-2xl">Projects by {user.name}</h1>
+      {projects === undefined ? (
+        <Spinner />
+      ) : (
+        <div className="flex justify-center">
+          <ProjectCardList projects={projects} />
+        </div>
+      )}
+    </>
+  )
+}
+
+export default CoderProjectsPage
