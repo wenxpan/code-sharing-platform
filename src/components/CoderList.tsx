1c3333b333c506fea7881ade56128a93d431e1d8
wenxuan-pan <panwenxuan42@gmail.com>
2024-02-23 23:57:37 +1100

    minor fixes

--- src/components/CoderList.tsx
+++ src/components/CoderList.tsx
@@ -5,19 +5,20 @@ import { Doc } from "@convex/_generated/dataModel"
 import { useQuery } from "convex/react"
 import { api } from "@convex/_generated/api"
 import ProfileCard from "./ProfileCard"
+import { Spinner } from "@nextui-org/react"
 
 interface CoderListProps {}
 
 const CoderList: React.FC<CoderListProps> = () => {
   const coders = useQuery(api.users.getCoders, {})
   if (!coders) {
-    return <p>Loading...</p>
+    return <Spinner />
   }
 
   // TODO: add pagination or infinite scroll
 
   return (
-    <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+    <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 place-items-stretch justify-items-stretch">
       {coders.map((coder: Doc<"users">) => (
         <li key={coder._id}>
           <ProfileCard coder={coder} />
