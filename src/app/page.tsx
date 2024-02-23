87b6134708830aedd354ff8974a108e2de728fc2
wenxuan-pan <panwenxuan42@gmail.com>
2024-02-23 14:18:28 +1100

    added text to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 import Link from "next/link"
 import React from "react"
 
-interface LandingPageProps { }
+interface LandingPageProps {}
 
 const LandingPage: React.FC<LandingPageProps> = () => {
   return (
@@ -11,25 +11,56 @@ const LandingPage: React.FC<LandingPageProps> = () => {
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
           <h1 className="my-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-            Boost your side projects
+            Elevate Your Coding Projects
           </h1>
           <div>
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Consequatur fuga cum qui explicabo animi rerum amet at ex.
-              Laudantium, adipisci!
+              Join a vibrant community of developers sharing, reviewing, and
+              improving web development projects together.
             </p>
-            <Button href='/projects' as={Link}>Get Started</Button>
+            <Button href="/login" as={Link} color="primary">
+              Get started
+            </Button>
           </div>
         </div>
       </section>
-      <section>
+      {/* <section>
         <Image
           src={"https://placehold.co/800x400/png"}
           alt="platform screenshot"
           width={800}
           height={400}
         />
+      </section> */}
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+          <h2 className="my-4 font-extrabold tracking-tight leading-none text-gray-900 text-2xl lg:text-6xl dark:text-white">
+            How it works
+          </h2>
+          <div>
+            <h3 className="text-xl">Share Projects</h3>
+            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+              Join a vibrant community of developers sharing, reviewing, and
+              improving web development projects together.
+            </p>
+          </div>
+          <div>
+            <h3 className="text-xl">Earn Points and Feedback</h3>
+            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+              Provide valuable feedback on projects to earn points. The more
+              points you have, the more visibility your projects get, ensuring
+              quality feedback from peers.
+            </p>
+          </div>
+          <div>
+            <h3 className="text-xl">Apply to Jobs with Your Portfolio</h3>
+            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+              Leverage your projects and the feedback you've received as a
+              dynamic portfolio to apply for job opportunities posted by top
+              tech companies on our platform.
+            </p>
+          </div>
+        </div>
       </section>
     </div>
   )
